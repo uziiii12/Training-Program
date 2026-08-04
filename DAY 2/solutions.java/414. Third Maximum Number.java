9fabaa79495c77e3d6810348be69abd52417ce33
uziiii12 <mohdozairkhan12@gmail.com>
2026-08-05 02:56:02 +0530

    added new solutions day 2

---
+++ DAY 2/solutions.java/414. Third Maximum Number.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+
+        Long first = null;
+        Long second = null;
+        Long third = null;
+
+        for (int num : nums) {
+
+            // Skip duplicates
+            if ((first != null && num == first) ||
+                (second != null && num == second) ||
+                (third != null && num == third)) {
+                continue;
+            }
+
+            if (first == null || num > first) {
+                third = second;
+                second = first;
+                first = (long) num;
+            }
+            else if (second == null || num > second) {
+                third = second;
+                second = (long) num;
+            }
+            else if (third == null || num > third) {
+                third = (long) num;
+            }
+        }
+
+        if (third == null) {
+            return first.intValue();
+        }
+
+        return third.intValue();
+    }
+}
