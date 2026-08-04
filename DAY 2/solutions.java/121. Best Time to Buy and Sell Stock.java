804f09ebeede2e38cfa054ea5a90c1c0050a0a71
uziiii12 <mohdozairkhan12@gmail.com>
2026-08-05 04:24:36 +0530

    added new solutions

---
+++ DAY 2/solutions.java/121. Best Time to Buy and Sell Stock.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int price : prices) {
+
+            if (price < minPrice) {
+                minPrice = price;
+            }
+
+            int profit = price - minPrice;
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+
+        return maxProfit;
+    }
+}
---
+++ DAY 2/solutions.py/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        minPrice = prices[0]
+        maxProfit = 0
+
+        for price in prices:
+
+            if price < minPrice:
+                minPrice = price
+
+            profit = price - minPrice
+
+            if profit > maxProfit:
+                maxProfit = profit
+
+        return maxProfit
