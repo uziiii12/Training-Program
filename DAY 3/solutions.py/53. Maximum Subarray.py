9d943ee550f7760a3d463d3135700d699598d224
uziiii12 <mohdozairkhan12@gmail.com>
2026-08-06 03:01:33 +0530

    added new solutions

---
+++ DAY 3/solutions.py/53. Maximum Subarray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        currentSum = nums[0]
+        maxSum = nums[0]
+
+        for i in range(1, len(nums)):
+
+            currentSum = max(nums[i], currentSum + nums[i])
+
+            maxSum = max(maxSum, currentSum)
+
+        return maxSum
