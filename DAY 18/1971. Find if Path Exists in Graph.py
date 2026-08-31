15017fef86fbd521682ff30e2e351aa509dd60c6
uziiii12 <mohdozairkhan12@gmail.com>
2026-08-31 11:32:40 +0530

    added new solution

--- DAY 18/1971. Find if Path Exists in Graph.py
+++ DAY 18/1971. Find if Path Exists in Graph.py
@@ -1,27 +1,20 @@
 class Solution:
-    def validPath(self, n, edges, source, destination):
-
-        graph = [[] for _ in range(n)]
-
-        for u, v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
-
-        stack = [source]
-        visited = set()
-
-        while stack:
-            node = stack.pop() 
-            if node == destination:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        adj=[]
+        for i in range(n):
+            adj.append([])
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        visited = {source}
+        queue = [source]
+        while queue:
+            curr = queue.pop(0)
+            if curr == destination:
                 return True
-
-            if node in visited:
-                continue
-
-            visited.add(node)
-
-            for neighbor in graph[node]:
+            for neighbor in adj[curr]:
                 if neighbor not in visited:
-                    stack.append(neighbor)
+                    visited.add(neighbor)
+                    queue.append(neighbor)
 
         return False
