091de772079f07e8566d5bb417ac208e8a848acb
uziiii12 <mohdozairkhan12@gmail.com>
2026-09-02 12:16:50 +0530

    added new solution

---
+++ DAY 15/78. Subsets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans=[]
+        curr=[]
+        def backtrack(start):
+            #ans.append(curr[:])
+            for i in range(start,len(nums)):
+                curr.append(nums[i])
+                backtrack(i+1)
+                curr.pop()
+            ans.append(curr[:])    
+        backtrack(0)
+        return ans        
+        
